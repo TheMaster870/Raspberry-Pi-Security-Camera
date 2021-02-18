10df884e07d6bbd6907afa61b5e3de743fb7128a
Drew Cameron <8572599+TheMaster870@users.noreply.github.com>
2021-02-18 14:45:26 +0000

    Update camerarecord.py

--- camerarecord.py
+++ camerarecord.py
@@ -16,7 +16,7 @@ while True:
     #Set the length of the recording by changing the number after -t (its in seconds)
     #-y overwrites the video if a file with the same name already exists (not likely to though)
     #-an means it doesn't record the audio (remove this if you want to add audio to the recording)
-    os.system("ffmpeg -f mjpeg -i http://127.0.0.1:8000/stream.mjpg -r 10 -t 60 " + videoname + " -y -an")
+    os.system("ffmpeg -f mjpeg -i http://127.0.0.1:8000/stream.mjpg -r 10 -t 300 " + videoname + " -y -an")
     print("done recording")
     #Move the recording to a NAS or SMB storage
     #This is setup for a share with no authentication, on a NAS called videosnas, a share called stuff and to save the recording in a folder called cctv
